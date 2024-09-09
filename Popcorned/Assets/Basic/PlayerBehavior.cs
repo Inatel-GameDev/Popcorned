24f052a14b65ad559626bc2462697123ac150f5f
Guilherme-souzae <guiemergente@gmail.com>
2024-09-09 03:46:06 -0300

    Sistema de pontos placeholder

--- Popcorned/Assets/Basic/PlayerBehavior.cs
+++ Popcorned/Assets/Basic/PlayerBehavior.cs
@@ -29,24 +29,66 @@ public class PlayerBehavior : MonoBehaviour
         // a cada frame sao coletados os inputs e as variaveis flags publicas sao atualizadas
         if (Input.GetMouseButtonDown(1))
         {
+            if (parringR)
+            {
+                Debug.Log("cancel");
+            }
+
             parringR = true;
             timer = Time.time;
         }
 
         if (parringR == true && (Time.time - timer) > tolerance)
         {
+            Debug.Log("calma");
             parringR = false;
         }
 
         if (Input.GetMouseButtonDown(0))
         {
+            if (parringL)
+            {
+                Debug.Log("cancel");
+            }
+
             parringL = true;
             timer = Time.time;
         }
 
         if (parringL == true && (Time.time - timer) > tolerance)
         {
+            Debug.Log("calma");
             parringL = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject pipocatemp = collision.gameObject;
+        if (pipocatemp.name == "PopcornR(Clone)")
+        {
+            if (parringR)
+            {
+                Debug.Log("acertou");
+                parringR = false;
+            }
+            else
+            {
+                Debug.Log("errou");
+            }
+        }
+
+        if (pipocatemp.name == "PopcornL(Clone)")
+        {
+            if (parringL)
+            {
+                Debug.Log("acertou");
+                parringL = false;
+            }
+            else
+            {
+                Debug.Log("errou");
+            }
         }
     }
 }
