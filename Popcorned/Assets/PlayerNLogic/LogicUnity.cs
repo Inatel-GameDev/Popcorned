d40f6666f2d703da058a8d0cb78e65af3006556a
Guilherme-souzae <guiemergente@gmail.com>
2024-12-12 02:57:07 -0300

    dicas visuais e melhora no codigo

--- Popcorned/Assets/PlayerNLogic/LogicUnity.cs
+++ Popcorned/Assets/PlayerNLogic/LogicUnity.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -9,6 +10,7 @@ public class LogicUnity : MonoBehaviour
     public static LogicUnity Instance;
 
     // dependencias
+    public SpriteRenderer leftArrow, rightArrow, upArrow, downArrow;
     public PopcornerBehavior popcornerR, popcornerL;
     public AudioSource src;
     public AudioClip cue;
@@ -37,6 +39,11 @@ public class LogicUnity : MonoBehaviour
     void Awake()
     {
         Instance = this;
+
+        leftArrow.enabled = false;
+        rightArrow.enabled = false;
+        upArrow.enabled = false;
+        downArrow.enabled = false;
     }
     void Start()
     {
@@ -53,30 +60,39 @@ public class LogicUnity : MonoBehaviour
         StartCoroutine(Loopar());
     }
     
-    private IEnumerator GiveAudioCue(int index, bool reverse)
+    private IEnumerator GiveCue(int index, bool reverse)
     {
         foreach (Vector2 nota in partitura[index])
         {
-            if (nota.y == 0)
+            if (nota.y == -1 ^ reverse)
             {
-                src.panStereo = reverse ? 1 : -1;
+                src.panStereo = -1;
+                StartCoroutine(ShowHide(leftArrow, 0.15f));
                 src.Play();
             }
 
-            if (nota.y == 1)
+            else if (nota.y == 1 ^ reverse)
             {
-                src.panStereo = reverse ? -1 : 1;
+                src.panStereo = 1;
+                StartCoroutine(ShowHide(rightArrow, 0.15f));
                 src.Play();
             }
 
             yield return new WaitForSeconds(nota.x / beat);
         }
     }
+    private IEnumerator ShowHide(SpriteRenderer sprite, float duration)
+    {
+        sprite.enabled = true;
+        yield return new WaitForSeconds(duration);
+        sprite.enabled = false;
+    }
+
     private IEnumerator StartPopcorning(int index, bool reverse)
     {
         foreach (Vector2 nota in partitura[index])
         {
-            if (nota.y == 0)
+            if (nota.y == -1)
                 popcornerL.TossPopcorn();
 
             if (nota.y == 1)
@@ -97,7 +113,7 @@ public class LogicUnity : MonoBehaviour
         Debug.Log($"Index = {index}");
         Debug.Log($"Reverse = {reverse}");
 
-        yield return StartCoroutine(GiveAudioCue(index, reverse));
+        yield return StartCoroutine(GiveCue(index, reverse));
 
         yield return StartCoroutine(StartPopcorning(index, reverse));
     }
@@ -115,32 +131,61 @@ public class LogicUnity : MonoBehaviour
         switch (currSpeed)
         {
             case CurrSpeed.slow:
-                currSpeed = (rng == 0) ? CurrSpeed.normal : CurrSpeed.fast;
-                beat = (rng == 0) ? normalBpm / 60f : fastBpm / 60f;
+                if (rng == 0)
+                {
+                    currSpeed = CurrSpeed.normal;
+                    beat = normalBpm / 60f;
+                }
+                else
+                {
+                    currSpeed = CurrSpeed.fast;
+                    beat = fastBpm / 60f;
+                }
+                StartCoroutine(ShowHide(upArrow, 1f));
                 break;
 
             case CurrSpeed.normal:
-                currSpeed = (rng == 0) ? CurrSpeed.slow : CurrSpeed.fast;
-                beat = (rng == 0) ? slowBpm / 60f : fastBpm / 60f;
+                if (rng == 0)
+                {
+                    currSpeed = CurrSpeed.slow;
+                    beat = slowBpm / 60f;
+                    StartCoroutine(ShowHide(downArrow, 1f));
+                }
+                else
+                {
+                    currSpeed = CurrSpeed.fast;
+                    beat = fastBpm / 60f;
+                    StartCoroutine(ShowHide(upArrow, 1f));
+                }
                 break;
 
             case CurrSpeed.fast:
-                currSpeed = (rng == 0) ? CurrSpeed.normal : CurrSpeed.slow;
-                beat = (rng == 0) ? normalBpm / 60f : slowBpm / 60f;
+                if (rng == 0)
+                {
+                    currSpeed = CurrSpeed.normal;
+                    beat = normalBpm / 60f;
+                }
+                else
+                {
+                    currSpeed = CurrSpeed.slow;
+                    beat = slowBpm / 60f;
+                }
+                StartCoroutine(ShowHide(downArrow, 1f));
                 break;
         }
     }
+
     private void GenSheet()
     {
         float roundendcue = spawndelay*beat;
 
-        partitura[0].Add(new Vector2(1f, 0));
+        partitura[0].Add(new Vector2(1f, -1));
         partitura[0].Add(new Vector2(0.5f, 1));
         partitura[0].Add(new Vector2(0.5f, 1));
         partitura[0].Add(new Vector2(1f, 1));
-        partitura[0].Add(new Vector2(0.5f, 0));
-        partitura[0].Add(new Vector2(0.5f, 0));
-        partitura[0].Add(new Vector2(roundendcue, 0));
+        partitura[0].Add(new Vector2(0.5f, -1));
+        partitura[0].Add(new Vector2(0.5f, -1));
+        partitura[0].Add(new Vector2(roundendcue, -1));
 
         partitura[1].Add(new Vector2(1f, 1));
         partitura[1].Add(new Vector2(1f, 1));
@@ -148,38 +193,38 @@ public class LogicUnity : MonoBehaviour
         partitura[1].Add(new Vector2(roundendcue, 1));
 
         partitura[2].Add(new Vector2(0.5f, 1));
-        partitura[2].Add(new Vector2(0.5f, 0));
+        partitura[2].Add(new Vector2(0.5f, -1));
         partitura[2].Add(new Vector2(0.5f, 1));
-        partitura[2].Add(new Vector2(0.5f, 0));
+        partitura[2].Add(new Vector2(0.5f, -1));
         partitura[2].Add(new Vector2(1f, 1));
-        partitura[2].Add(new Vector2(roundendcue, 0));
+        partitura[2].Add(new Vector2(roundendcue, -1));
 
-        partitura[3].Add(new Vector2(1f, 0));
-        partitura[3].Add(new Vector2(1f, 0));
+        partitura[3].Add(new Vector2(1f, -1));
+        partitura[3].Add(new Vector2(1f, -1));
         partitura[3].Add(new Vector2(0.5f, 1));
         partitura[3].Add(new Vector2(0.5f, 1));
-        partitura[3].Add(new Vector2(roundendcue, 0));
+        partitura[3].Add(new Vector2(roundendcue, -1));
 
-        partitura[4].Add(new Vector2(0.5f, 0));
+        partitura[4].Add(new Vector2(0.5f, -1));
         partitura[4].Add(new Vector2(0.5f, 1));
-        partitura[4].Add(new Vector2(0.5f, 0));
+        partitura[4].Add(new Vector2(0.5f, -1));
         partitura[4].Add(new Vector2(0.5f, 1));
-        partitura[4].Add(new Vector2(1f, 0));
+        partitura[4].Add(new Vector2(1f, -1));
         partitura[4].Add(new Vector2(roundendcue, 1));
 
-        partitura[5].Add(new Vector2(1f, 0));
+        partitura[5].Add(new Vector2(1f, -1));
         partitura[5].Add(new Vector2(0.5f, -1));
-        partitura[5].Add(new Vector2(1f, 0));
+        partitura[5].Add(new Vector2(1f, -1));
         partitura[5].Add(new Vector2(0.5f, 1));
         partitura[5].Add(new Vector2(0.5f, 1));
         partitura[5].Add(new Vector2(roundendcue, 1));
 
-        partitura[6].Add(new Vector2(1f, 0));
+        partitura[6].Add(new Vector2(1f, -1));
         partitura[6].Add(new Vector2(0.5f, 1));
         partitura[6].Add(new Vector2(0.5f, 1));
         partitura[6].Add(new Vector2(1f, 1));
-        partitura[6].Add(new Vector2(0.5f, 0));
-        partitura[6].Add(new Vector2(0.5f, 0));
-        partitura[6].Add(new Vector2(roundendcue, 0));
+        partitura[6].Add(new Vector2(0.5f, -1));
+        partitura[6].Add(new Vector2(0.5f, -1));
+        partitura[6].Add(new Vector2(roundendcue, -1));
     }
 }
